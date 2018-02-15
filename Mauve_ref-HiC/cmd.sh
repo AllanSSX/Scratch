6de96c23258f00b9eb30c582926a218472fbfff5
AllaSSX <cormieralexandre@gmail.com>
2018-02-15 13:43:56 +0100

    first commit

--- Mauve_ref-HiC/cmd.sh
+++ Mauve_ref-HiC/cmd.sh
@@ -9,11 +9,11 @@ for fasta in *.fasta; do fasta2gbk.py -f ${fasta} -g ${fasta%.*}.gff3 -o ${fasta
 
 #HiC
 split_fasta.py -f Ectsi_genome_HiC.fasta
-rename.pl 's/-assembly|/_/' *.fasta
+rename.pl 's/-assembly\|/_/' *.fasta
 sed -i 's/-assembly|/_/' 3C_*.fasta
 
 info_frags2gff3.py -i info_frags.txt | sed 's/-assembly|/_/' > chr_HiC.gff3
-for i in $(cut -f1 chr_sctg.gff3 | sort | uniq); do grep ${i} chr_HiC.gff3 > ${i}.gff3; done
+for i in $(cut -f1 chr_HiC.gff3 | sort | uniq); do grep ${i} chr_HiC.gff3 > ${i}.gff3; done
 
 for fasta in *.fasta; do fasta2gbk.py -f ${fasta} -g ${fasta%.*}.gff3 -o ${fasta%.*}; done
 
