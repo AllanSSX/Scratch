c46ab32db5b8f83a43e9034a834a750fb07bd9eb
AllaSSX <cormieralexandre@gmail.com>
2018-05-28 14:09:14 +0200

    output formatting

--- GC.py
+++ GC.py
@@ -6,6 +6,7 @@ from Bio import SeqIO
 def getArgs():
 	parser = argparse.ArgumentParser(description="")
 	parser.add_argument('-f',dest="fasta",type=str,required=True,help='fasta')
+	parser.add_argument('-d',dest="detail", action='store_true', help='Print %GC for each sequence')
 	
 	arg = parser.parse_args()
 	
@@ -25,7 +26,8 @@ def main(args):
 		A = nucl['A'] + nucl['a']; C = nucl['C'] + nucl['c']; G = nucl['G'] + nucl['g']; T = nucl['T'] + nucl['t']; N = nucl['N'] + nucl['n']
 		size = A + G + C + T		
 		GC = float((G + C)) / float(size) * 100
-		print(seq.id, GC)
+		if args.detail:
+			print(str(seq.id)+'\t'+str(GC))
 		GCs.append(GC)
 	GC = round(sum(GCs) / float(len(GCs)), 2)
 	print('Global GC value:' + str(GC))
