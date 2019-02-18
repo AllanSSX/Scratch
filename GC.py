6c7f443306e27988bff4101e72ccf0ea644db5ca
AllaSSX <cormieralexandre@gmail.com>
2019-02-18 09:55:28 +0100

    mean to exact value

--- GC.py
+++ GC.py
@@ -6,7 +6,7 @@ from Bio import SeqIO
 def getArgs():
 	parser = argparse.ArgumentParser(description="")
 	parser.add_argument('-f',dest="fasta",type=str,required=True,help='fasta')
-	parser.add_argument('-d',dest="detail", action='store_true', help='Print %GC for each sequence')
+	parser.add_argument('-d',dest="detail", action='store_true', help='Print GC for each sequence')
 	
 	arg = parser.parse_args()
 	
@@ -14,7 +14,7 @@ def getArgs():
 
 def main(args):
 	
-	GCs = []
+	nuclFasta = {'A':0, 'a':0,'C':0, 'c':0,'G':0, 'g':0,'T':0, 't':0,'N':0, 'n':0}
 	for seq in SeqIO.parse(args.fasta, "fasta"):
 		
 		nucl = {'A':0, 'a':0,'C':0, 'c':0,'G':0, 'g':0,'T':0, 't':0,'N':0, 'n':0}
@@ -22,15 +22,18 @@ def main(args):
 		for codon in sequence:
 			if codon in nucl:
 				nucl[codon] += 1
+				nuclFasta[codon] += 1
 				
 		A = nucl['A'] + nucl['a']; C = nucl['C'] + nucl['c']; G = nucl['G'] + nucl['g']; T = nucl['T'] + nucl['t']; N = nucl['N'] + nucl['n']
 		size = A + G + C + T		
 		GC = float((G + C)) / float(size) * 100
 		if args.detail:
 			print(str(seq.id)+'\t'+str(GC))
-		GCs.append(GC)
-	GC = round(sum(GCs) / float(len(GCs)), 2)
-	print('Global GC value:' + str(GC))
+	
+	A = nuclFasta['A'] + nuclFasta['a']; C = nuclFasta['C'] + nuclFasta['c']; G = nuclFasta['G'] + nuclFasta['g']; T = nuclFasta['T'] + nuclFasta['t']
+	size = A + G + C + T		
+	GC = float((G + C)) / float(size) * 100
+	print('Global GC value:' + str(round(GC,2)))
 
 if __name__ == '__main__':
 	args = getArgs()
